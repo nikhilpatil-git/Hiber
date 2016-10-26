bb9c409deb2486d48d2743c97be1c4314cc4a707
nikhilpatil-git <amoona--2005@hotmail.com>
2016-10-26 10:49:41 +0100

    new changes

--- src/main/java/com/edubot/entities/AbstractQuestion.java
+++ src/main/java/com/edubot/entities/AbstractQuestion.java
@@ -1,7 +1,49 @@
 package com.edubot.entities;
 
+import java.io.Serializable;
+
 /**
  * Created by deadcode on 26/10/2016.
  */
-public class AbstractQuestion {
+public abstract class AbstractQuestion implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    private long abstractQuestionId;
+    private String text;
+    private String hint;
+    private int rightAnswer;
+    private int sequence;
+
+
+    public long getAbstractQuestionId() {
+        return abstractQuestionId;
+    }
+
+    public void setAbstractQuestionId(long abstractQuestionId) {
+        this.abstractQuestionId = abstractQuestionId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    public int getRightAnswer() {
+        return rightAnswer;
+    }
+
+    public void setRightAnswer(int rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
 }
---
+++ src/main/java/com/edubot/entities/TrueFalseQuestion.java
@@ -0,0 +1,14 @@
+package com.edubot.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrueFalseQuestion extends AbstractQuestion implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    public TrueFalseQuestion() {
+    }
+}
